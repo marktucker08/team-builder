1b80c0dbd4eb6a235a514c692abe0354f1d13a21
Mark Tucker <markdaddy@Marks-MacBook-Air.local>
2022-10-25 20:17:23 -0600

    added app basic functionality and initial team member state

--- src/App.js
+++ src/App.js
@@ -4,10 +4,22 @@ import React, { useState } from 'react';
 
 import Form from './Form';
 
+const teamMemberList = [{name: 'Mark', email: 'mark@me.com', role: "Software Engineer"},]
+
 function App() {
+  const [members, setMembers] = useState(teamMemberList);
   return (
     <div className="App">
+      <h1>New Team Member Form</h1>
+
       <Form />
+      <h2>Team Members:</h2>
+      {members.map((member, idx) => {
+        return (
+        <div key={idx}>
+          <li>Name: {member.name}, Email: {member.email}, Role: {member.role}</li>
+        </div>
+      )})}
     </div>
   );
 }
